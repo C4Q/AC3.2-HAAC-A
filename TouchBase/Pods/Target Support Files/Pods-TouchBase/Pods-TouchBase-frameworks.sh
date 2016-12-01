2553f7ac67ecbb50a68d45724d44f3cd2c106d68
Cristopher Chavez <cristopherchavezny@gmail.com>
2016-12-01 17:26:50 -0500

    updating pod 'MRArticleViewController'

--- TouchBase/Pods/Target Support Files/Pods-TouchBase/Pods-TouchBase-frameworks.sh
+++ TouchBase/Pods/Target Support Files/Pods-TouchBase/Pods-TouchBase-frameworks.sh
@@ -84,12 +84,16 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/ASCollectionView/ASCollectionView.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/CHTCollectionViewWaterfallLayout/CHTCollectionViewWaterfallLayout.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MRArticleViewController/MRArticleViewController.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/ASCollectionView/ASCollectionView.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/CHTCollectionViewWaterfallLayout/CHTCollectionViewWaterfallLayout.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MRArticleViewController/MRArticleViewController.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
 fi
--- TouchBase/TouchBase/ViewController.swift
+++ TouchBase/TouchBase/ViewController.swift
@@ -7,21 +7,78 @@
 //
 
 import UIKit
+import ASCollectionView
 
-class ViewController: UIViewController {
-    var sources: [Sources] = []
+class ViewController: UIViewController, ASCollectionViewDataSource, ASCollectionViewDelegate  {
+    var newsSources: [Sources] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         getsources()
     }
     
     func getsources() {
-       APIManager.shared.makeRequest { (sources) in
-            print(sources![0].name)
+        APIManager.shared.makeRequest { (sources) in
+            if let validSources = sources {
+                self.newsSources = validSources
+            }
         }
     }
+    func downloadImageFrom(urlString: String, completion: @escaping (_ image: UIImage?) -> Void) {
+        guard let url = URL(string: urlString) else { return }
+        let session = URLSession(configuration: .default)
+        session.dataTask(with: url) { (data, response, error) in
+            guard let validData = data, let image = UIImage(data: validData) else {
+                //                print(error?.localizedDescription)
+                completion(nil)
+                
+                return
+            }
+            DispatchQueue.main.async {
+                completion(image)
+            }
+        }
+    }
+    
 
-
-
+    
+    // MARK: ASCollectionViewDataSource
+    
+    func numberOfItemsInASCollectionView(_ asCollectionView: ASCollectionView) -> Int {
+        return sources.count
+    }
+    
+    func collectionView(asCollectionView: ASCollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let gridCell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as! GridCell
+        let news = sources[indexPath.row]
+        let imageURL = news.sourceLogos.mediumLogo
+//        gridCell.label.text = NSString(format: "Item %ld ", indexPath.row) as String
+        downloadImageFrom(urlString: imageURL) {(image: UIImage?) in
+        gridCell.imageView.image = image
+        return gridCell
+    }
+    
+    func collectionView(asCollectionView: ASCollectionView, parallaxCellForItemAtIndexPath indexPath: NSIndexPath) -> ASCollectionViewParallaxCell {
+        let parallaxCell = collectionView.dequeueReusableCellWithReuseIdentifier("parallaxCell", forIndexPath: indexPath) as! ParallaxCell
+        parallaxCell.label.text = NSString(format: "Item %ld ", indexPath.row) as String
+        parallaxCell.updateParallaxImage(UIImage(named: NSString(format: "image-%ld", indexPath.row % 10) as String)!)
+        return parallaxCell
+    }
+    
+    func collectionView(asCollectionView: ASCollectionView, headerAtIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryViewOfKind(ASCollectionViewElement.Header, withReuseIdentifier: "header", forIndexPath: indexPath)
+        return header
+    }
+    
+    func loadMoreInASCollectionView(asCollectionView: ASCollectionView) {
+        if numberOfItems > 30 {
+            collectionView.enableLoadMore = false
+            return
+        }
+        numberOfItems += 10
+        collectionView.loadingMore = false
+        collectionView.reloadData()
+    }
+    
 }
 
